26d75b1799bc8b90451e8909c7c1dec02adf00f3
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-11-04 20:11:33 -0500

    forgot to save?

--- bank.c
+++ bank.c
@@ -14,26 +14,69 @@ typedef struct account_t{
    int accountBal;
 } account_t;
 
+int menu()
+{
+   int status;
+   printf("Welcome to the bank!\nPlease make a selection:\n")
+   printf("0: Exit\n1: Deposit\n2: Withdrawal\n3: Add Account\n");
+   prtinf("4: Remove Account\n5: Balance Inquiry\n6: View Accounts\n");
+   scanf("%i", status);
+   return status;
+}
+
 // A menu-driven banking program storing data in a binary file
 int main(int argc, char** argv)
 {
+   //declare variables and open the file
    FILE * fp;
    account_t account;
-   int accNumber = 0;
-   int amount = 0;
+   account_t accList[49];
+   int temp_accNum;
+   int temp_amt;
    int readCheck;
+   int i = 0;
+   int numOfAccts = 0;
    fp = fopen("accounts.dat", "ab+");
    if(fp == NULL)
    {
       printf("Failed to open the file!\n");
       return 1;
    }
+
+   //make an array of everything in the file so far
+   readCheck = fread(&account, size_of(account_t), 1, fp);
+   while( (readCheck != 0) && (i < 50) )
+   {
+      accList[i] = account;
+      i++;
+      readCheck = fread(&account, size_of(account_t), 1, fp);
+   }
+   numOfAccts = i; //get the current max number of accounts
+
+   //start the main loop
+   //print menu, get user selection
    int status = menu();
+   //exit program on 0
    while(status != 0)
    {
-      switch(status)
+      switch(status) //switch on the option chosen
       {
          case 1: //deposit
+            printf("Please enter the account number:\n");
+            scanf("%i", temp_accNum);
+            for( i = 0; i <= numOfAccts; i++)
+            {
+               if (accList[i].accountNum == temp_accNum)
+               {
+                  printf("Please enter the amount to deposit\n");
+                  scanf("%i", temp_amt);
+                  accList[i].accountBal = temp_amt;
+                  printf("Deposit successful!\n");
+                  status = menu();
+                  break;
+               }
+            }
+            printf("Account number is invalid!\n");
             status = menu();
             break;
          case 2: //withdraw
@@ -51,7 +94,11 @@ int main(int argc, char** argv)
          case 6: //view all accounts
             status = menu();
             break;
+         default:
+            printf("Invalid selection!");
+            status = menu();
       }
    }
    fclose(fp);
+   return 0;
 }
